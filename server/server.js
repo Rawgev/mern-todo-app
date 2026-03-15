7bc422241650b39af93e98b440340e35abd0cdef
Raghav <raghavchauhan179@gmail.com>
2026-03-16 03:30:30 +0530

    Fix CORS configuration

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const Todo = require("./models/Todo");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*"
+  })
+);
 app.use(express.json());
 
 const config = {
